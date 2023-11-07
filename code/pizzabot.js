85fcaa07e396d5dbcbff107c3023858ff543ce48
nurgultekin <92712594+nurgultekin@users.noreply.github.com>
2023-11-07 14:57:05 +0100

    -

---
+++ code/pizzabot.js
@@ -0,0 +1,25 @@
+const vegetarian = "Vegetarian";
+const hawaiian = "Hawaiian";
+const pepperoni = "Pepperoni";
+
+const pizzaPrice = 80;
+
+//Put your Javscript code here:
+
+alert("Hey! Happy to serve your pizza. On our menu we have " + vegetarian ", " + hawaiian + " and " + pepperoni)
+
+
+const orderName = prompt("Enter the name of the pizza you want to order today", )
+const orderQuantity = prompt("How many of pizzas you want?")
+
+if (
+    orderName == vegetarian ||
+    orderName == hawaiian ||
+    orderName == pepperoni ||
+);
+
+console.log(orderQuantity)
+
+ 
+
+
